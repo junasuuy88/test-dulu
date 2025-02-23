dbaa53da4cf845556ba557440774da1981f7c2b7
junasuuy88 <junasuuy@gmail.com>
2025-02-23 20:31:48 +0700

    Add files via upload

---
+++ oke.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html amp lang="id">
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="description" itemprop="description" content="Otaku88 adalah platfrom judi online terkemuka resmi untuk permainan slot gacor terpercaya di Indonesia." />
+    <meta name="keywords" content="otaku88, otaku 88, otaku88 login, situs otaku88, otaku88 alternatif" />
+    <title>OTAKU88: Link Situs Slot Judi Online paling Gacor Terbaru & Terpercaya</title>
+    <!-- Canonical Blade -->
+    <link rel="canonical" href="https://coneyislandmusicfestival.com/" />
+    <!-- Custom Tags Blade -->
+    
+    <!-- End of Custom Blade -->
+    <link rel="shortcut icon" type="image/png" href="fav.png">
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes  -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+
+    <style amp-custom>
+        body {font-family: 'Open Sans', sans-serif;background: #121212;background-size: cover;background-position: center;background-repeat: no-repeat;background-attachment: fixed;padding-bottom: 80px;}.header-mobile {position: sticky;top: 0;z-index: 1;padding: 10px 10px;display: flex;align-items: center;background: #000000;border-bottom: 1px solid #fdbf29;}.header-mobile .mobile-logo {min-height: 30px;}.header-mobile .mobile-logo amp-img img {width: 100%;height: auto;object-fit: contain;object-position: left;}.mobile-button {margin-left: auto;display: flex;align-items: center;}.mobile-button button {outline: none;border-radius: 15px;min-width: 75px;min-height: 33px;border: none;font-size: 14px;font-weight: 600;margin-left: 5px;padding: 5px 0px;box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25);}.btn-masuk{background:radial-gradient(circle 214px at 46.5% 54.2%,#ffc107 0,#ffc837 90%)}.login a{text-decoration: none;color: #000000;}.mobile-button .register {background: linear-gradient(to bottom, #f12222 0%, #ae0017 100%);text-decoration: none;}.mobile-button .register a {color: #ffff;text-decoration: none;}.menu-games {display: grid;grid-template-columns: repeat(2, 1fr);grid-gap: 15px;margin: 20px 0;}.menu-games a {text-decoration: none;}.games-item {background: transparent;text-align: center;}.games-item amp-img {animation: bounce 2s infinite alternate;}.games-item amp-img img {object-fit: contain;}.game-border {background: #987225;padding: 2px;border-radius: 20px;margin-top: 10px;max-width: 200px;margin-left: auto;margin-right: auto;}.games-name {background: linear-gradient(to bottom, #fdbf29 0%, #fdc965 31%, #987225 66%, #6c4e13 100%);font-size: 13px;padding: 6px 5px;font-weight: 700;color: #000000;border-radius: 20px;}@keyframes bounce {0% {transform: translateY(-3px)}50% {transform: translateY(3px)}100% {transform: translateY(0)}}.footer-mobile {background: #030303;border-top: 2px solid #fcd94f;position: fixed;bottom: 0;right: 0;left: 0;z-index: 997;display: grid;grid-template-columns: repeat(4, 1fr);}.footer-mobile .footer-item {text-align: center;padding: 12px 5px 10px;border-right: 2px solid #fcd94f;color: #fff;text-decoration: none;}.footer-mobile .footer-item .footer-icon i {font-size: 20px;}.footer-mobile .footer-item .footer-title {font-size: 13px;font-weight: 600;}.shake {-webkit-animation-name:shake;animation-name:shake;-webkit-animation-duration:4.5s;animation-duration:1.5s;-webkit-animation-fill-mode:both;animation-fill-mode:both;animation-iteration-count:infinite}@-webkit-keyframes shake{0%,100%{-webkit-transform:scale3d(1,1,1);transform:scale3d(1,1,1)}10%,20%{-webkit-transform:scale3d(.9,.9,.9) rotate3d(0,0,1,-3deg);transform:scale3d(.9,.9,.9) rotate3d(0,0,1,-3deg)}30%,50%,70%,90%{-webkit-transform:scale3d(1.1,1.1,1.1) rotate3d(0,0,1,3deg);transform:scale3d(1.1,1.1,1.1) rotate3d(0,0,1,3deg)}40%,60%,80%{-webkit-transform:scale3d(1.1,1.1,1.1) rotate3d(0,0,1,-3deg);transform:scale3d(1.1,1.1,1.1) rotate3d(0,0,1,-3deg)}}@keyframes shake{0%,100%{-webkit-transform:scale3d(1,1,1);transform:scale3d(1,1,1)}10%,20%{-webkit-transform:scale3d(.9,.9,.9) rotate3d(0,0,1,-3deg);transform:scale3d(.9,.9,.9) rotate3d(0,0,1,-3deg)}30%,50%,70%,90%{-webkit-transform:scale3d(1.1,1.1,1.1) rotate3d(0,0,1,3deg);transform:scale3d(1.1,1.1,1.1) rotate3d(0,0,1,3deg)}40%,60%,80%{-webkit-transform:scale3d(1.1,1.1,1.1) rotate3d(0,0,1,-3deg);transform:scale3d(1.1,1.1,1.1) rotate3d(0,0,1,-3deg)}}.dftr {animation: wiggle 2s linear infinite;}@keyframes wiggle {0%, 7% {transform: rotateZ(0);}15% {transform: rotateZ(-15deg);}20% {transform: rotateZ(10deg);}25% {transform: rotateZ(-10deg);}30% {transform: rotateZ(6deg);}35% {transform: rotateZ(-4deg);}40%, 100% {transform: rotateZ(0);}}
+        .home__seo {padding: 0 15px;margin-top: 25px;}.home__seo .game__seo {background: linear-gradient(to bottom, #696969 0%, #575757 100%);;border: 2px solid #575757;color: #ffffff;border-radius: 10px;padding: 15px 0;}.home__seo .game__seo .seo-content {padding: 0 15px 10px;font-size: 16px;text-align: justify;}.home__seo .seo-mobile .seo-mob-title{background: transparent;text-align: center;padding: 10px 15px;border: 0;border-radius: 10px;font-size: 16px;font-weight: 600;margin: 0;}.home__seo .seo-mobile .seo-mob-content {padding: 0 15px 10px;font-size: 16px;text-align: justify;}.home__seo .seo-mobile .seo-mob-content h1 {font-size: 1.25rem;}.home__seo .seo-mobile .seo-mob-content h2 {font-size: 1.2rem;}.home__seo .seo-mobile .seo-mob-content h3 {font-size: 1.15rem;}.home__seo .seo-mobile .seo-mob-content h4 {font-size: 1.1rem;}.home__seo .seo-mobile .seo-mob-content h5 {font-size: 1.05rem;}.home__seo .seo-mobile .seo-mob-content h6 {font-size: 1rem;}.home__seo .seo-mobile .seo-mob-content p {font-size: 14px;}
+    </style>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,500;0,600;0,700;1,400&display=swap">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" type="text/css"/>
+    <script async custom-element="amp-accordion" src="https://cdn.ampproject.org/v0/amp-accordion-0.1.js"></script>
+    <script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.2.js"></script>
+    <script async custom-element="amp-lightbox" src="https://cdn.ampproject.org/v0/amp-lightbox-0.1.js"></script>
+    <script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>
+    <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+        <script type="application/ld+json">
+                    {
+                "@context": "https://schema.org",
+                "@type": "WebSite",
+                "name": "OTAKU88: Link Situs Slot Judi Online paling Gacor Terbaru & Terpercaya",
+                "url": "https://coneyislandmusicfestival.com/"
+            }
+            </script>
+</head>
+
+<body>
+    <div class="header-mobile">
+        <div class="mobile-logo">
+            <a href="#">
+                <amp-img src="otaku88.png" alt="WebsiteLogo" width="140" height="30" layout="intrinsic">
+                </amp-img>
+            </a>
+        </div>
+        <div class="mobile-button">
+            <button class="btn-masuk login" type="button">
+                <a href="https://odinurl.com/CFRmScu">Masuk</a>
+            </button>
+            <button class="register dftr">
+                <a href="https://odinurl.com/rMUfCGj">Daftar</a>
+            </button>
+        </div>
+    </div>
+
+    <div class="main-content home">
+        <div class="home__slider">
+            <amp-carousel  width="720" height="360"  layout="responsive" type="slides" autoplay delay="2000">
+                <a href="#">
+                    <amp-img src="1.jpg"  width="720" height="360"  layout="responsive" alt=""></amp-img>
+                </a>
+                <a href="#">
+                    <amp-img src="2.jpg"  width="720" height="360"  layout="responsive" alt=""></amp-img>
+                </a>
+                <a href="#">
+                    <amp-img src="3.jpg"  width="720" height="360"  layout="responsive" alt=""></amp-img>
+                </a>
+            </amp-carousel>
+        </div>
+        <div class="home__menu">
+            <div class="menu-games">
+                 <a href="https://odinurl.com/CFRmScu">
+                    <div class="games-item">
+                        <amp-img src="slot.png" width="138" height="100" alt="slot"></amp-img>
+                        <div class="game-border">
+                            <div class="games-name">slot</div>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://odinurl.com/CFRmScu">
+                    <div class="games-item">
+                        <amp-img src="sportsbook.png" width="138" height="100" alt="sportsbook"></amp-img>
+                        <div class="game-border">
+                            <div class="games-name">Sportsbook</div>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://odinurl.com/CFRmScu">
+                    <div class="games-item">
+                        <amp-img src="casino.png" width="138" height="100" alt="casino"></amp-img>
+                        <div class="game-border">
+                            <div class="games-name">Casino</div>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://odinurl.com/CFRmScu">
+                    <div class="games-item">
+                        <amp-img src="lottery.png" width="138" height="100" alt="lottery"></amp-img>
+                        <div class="game-border">
+                            <div class="games-name">Lottery</div>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://odinurl.com/CFRmScu">
+                    <div class="games-item">
+                        <amp-img src="arcade.png" width="138" height="100" alt="arcade"></amp-img>
+                        <div class="game-border">
+                            <div class="games-name">Arcade</div>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://odinurl.com/CFRmScu">
+                    <div class="games-item">
+                        <amp-img src="live.png" width="138" height="100" alt="livegames"></amp-img>
+                        <div class="game-border">
+                            <div class="games-name">Live Game</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+      
+    </div>
+
+    <div class="home__seo">
+        <div class="game__seo">
+            <div class="seo-content showFooter">
+                <h1>OTAKU88: Link Situs Slot Judi Online paling Gacor Terbaru & Terpercaya</h1>
+                <p><a href="https://coneyislandmusicfestival.com/">Otaku88</a> adalah platfrom judi online terkemuka resmi untuk permainan slot gacor terpercaya di Indonesia. Situs ini menghadirkan kenyamanan dan kemudahan serta keamanna bagi penggemar <a href="https://coneyislandmusicfestival.com/">slot gacor hari ini</a>. Dengan sistem canggih dan layanan pelanggan 24/7, otaku88 menjamin pengalaman bermain slot bakalan jadi lebih seru dan adil.</p>
+                <p>Bergabung dengan situs <a href="https://coneyislandmusicfestival.com/">otaku 88</a> memberikan akses mudah untuk bermain slot gacor terbaru hari ini. Platform ini menjadi pilihan situs slot terpercaya bagi pecinta permainan jackpot. Mereka mencari sensasi bermain di situs slot resmi dan terpercaya ini adalah tempat yang tepat.</p>
+                <h2>Tersedia Ratusan Link Game Slot Gacor Garansi Maxwin</h2>
+                <p>Otaku88 adalah situs slot yang menyediakan ratusan game gacor yang resmi dari berbagai provider ternama seperti pragmatic play, pgsoft, microgaming, joker, habanero dan banyak lagi. Hebatnya lagi kalian cuma butuh 1 akun untuk akses kesemua game slot tersebut.</p>
+                <h2>Jaminan Pembayaran Kemenangan 100%</h2>
+                <p>Otaku88 selaku <a href="https://coneyislandmusicfestival.com/">situs slot resmi</a> menjamin semua kemenangan dari member akan dibayarkan tepat waktu dan aman sampai kerekening member. Karena kami paham betul bahwa setiap member memerlukan kepastian pembayaran sehingga semua sudah diatur oleh ketentuan berlaku.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-mobile">
+        <a class="footer-item" href="https://odinurl.com/CFRmScu">
+            <div class="footer-icon"><amp-img src="log.png" alt="login" height="23" width="24" layout="intrinsic"></div>
+            <div class="footer-title">Masuk</div>
+        </a>
+        <a class="footer-item" href="https://odinurl.com/rMUfCGj">
+			<div class="footer-icon"><amp-img src="regis.png" alt="login" height="23" width="24" layout="intrinsic"></div>
+            <div class="footer-title">Daftar</div>
+        </a>
+        <a class="footer-item" href="https://app.chaport.com/widget/show.html?appid=662ce793955c5da50199c19c">
+            <div class="footer-icon shake"><amp-img src="chat.png" alt="login" height="23" width="24" layout="intrinsic"></i></div>
+            <div class="footer-title">Live Chat</div>
+        </a>
+        <a class="footer-item" href="https://odinurl.com/eUaYkWP">
+            <div class="footer-icon"><amp-img src="wa.png" alt="login" height="23" width="24" layout="intrinsic"></div>
+            <div class="footer-title">Whatsapp</div>
+        </a>
+    </div>
+</body>
